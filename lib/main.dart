2b7f343a29675892248e5cdd17c0bf716c5b5683
Samuel Alapakristi <samalapsy@gmail.com>
2020-04-10 14:07:31 +0100

    Added Audio Player Dependency Play sounds Create a function that returned Expanded widget of each note.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,41 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  int play(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildKey(int note, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () => play(note),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.blue),
+              buildKey(6, Colors.indigo),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
